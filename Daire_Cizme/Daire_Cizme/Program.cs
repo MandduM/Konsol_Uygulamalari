ba5c5eb6b264114a401986be708b0838ce620d89
Ali Kaan Ekmekci <ekmekcialikaan@gmail.com>
2023-06-22 10:32:17 +0300

    Daire Çizme Projesi Bitti

---
+++ Daire_Cizme/Daire_Cizme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daire_Cizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dairenin yarıçapını girin: ");
+            double yaricap = Convert.ToDouble(Console.ReadLine());
+
+            DaireCizici daireCizici = new DaireCizici();
+            daireCizici.DaireCiz(yaricap);
+
+            Console.ReadLine();
+        }
+    }
+
+    class DaireCizici
+    {
+        public void DaireCiz(double yaricap)
+        {
+            double daireCevresi = 2 * Math.PI * yaricap;
+            double daireAlanı = Math.PI * yaricap * yaricap;
+
+            Console.WriteLine("Daire Çizildi!");
+            Console.WriteLine($"Yarıçap: {yaricap}");
+            Console.WriteLine($"Çevre: {daireCevresi}");
+            Console.WriteLine($"Alan: {daireAlanı}");
+        }
+    }
+}
