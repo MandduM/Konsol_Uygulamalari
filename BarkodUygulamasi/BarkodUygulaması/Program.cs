4f03c620d63f68475f2b0b5ba4263eb3dcf01334
Ali Kaan Ekmekci <ekmekcialikaan@gmail.com>
2023-06-23 20:51:32 +0300

    Barkod Projesi Bitti

---
+++ BarkodUygulamasi/BarkodUygulaması/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using BarcodeLib;
+using BarcodeStandard;
+using ZXing;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BarcodeGenerator
+{
+    public class BarcodeActions
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Barkod yazdırmak için metni girin: ");
+            string text = Console.ReadLine();
+
+            writeBarcode(text);
+            readBarcode();
+        }
+
+        static void writeBarcode(string word)
+        {
+            Barcode barcode = new Barcode();
+
+
+            Image img = barcode.Encode(TYPE.CODE128, word); //Barkodu resme dönüştürme
+
+            Bitmap bmp = new Bitmap(img);// Barkod resmini Bitmap'e dönüştürme
+
+            string filePath = @"D:\Emirin yazılım şeysileri\CSharp--101\BarcodeGenerator\BarcodeGenerator\barcode.png";
+
+            bmp.Save(filePath);// Barkodu dosyaya kaydetme
+        }
+
+
+        static void readBarcode()
+        {
+
+            BarcodeReader barcodeReader = new BarcodeReader(); // Barkod okuyucu nesnesi oluşturma
+
+            barcodeReader.Options.TryHarder = true; //Daha doğru okuma için
+
+            var barcodeBitMap = new Bitmap(@"D:\Emirin yazılım şeysileri\CSharp--101\BarcodeGenerator\BarcodeGenerator\barcode.png");
+
+            var result = barcodeReader.Decode(barcodeBitMap); // Barkodu okuma
+
+            if (result != null) // Eğer barkod okunamazsa sonuç null olacaktır
+                Console.WriteLine("Barkod içeriği: " + result.Text);
+            else
+                Console.WriteLine("Barkod okunamadı.");
+
+
+            // Konsol penceresini açık tutmak için
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BarkodUygulamasi/BarkodUygulaması/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("BarkodUygulaması")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BarkodUygulaması")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a469e3e9-3f9a-4fa4-9ebc-ec3aa2e2fef0")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
